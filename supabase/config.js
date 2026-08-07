00ce7d8a9d48bebdf796818bd3f94790ad46524b
marciobignon <marciobignon@gmail.com>
2026-08-07 15:45:01 -0300

    Update config.js

--- supabase/config.js
+++ supabase/config.js
@@ -2,7 +2,7 @@
 
 const SUPABASE_URL = "https://biybukmmwrrhvwsgwqff.supabase.co";
 
-const SUPABASE_ANON_KEY = "sb_publishable_e8ZNRG4-tRkiGHCV2Dx_Dw_ZrvXdOt7";
+const SUPABASE_ANON_KEY = "sb_publishable_e8ZNRG4-tRkiGHCV2Dx_Dw_ZrvXdOt7;
 
 const supabaseClient = supabase.createClient(
     SUPABASE_URL,
